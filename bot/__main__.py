7c178851f879c85812b279226a973d1b9daa771e
amo-incognito <>
2021-04-13 13:33:03 +0200

    Minor improvements
    - blackify code
    - remove whitespaces
    - clean up imports
    - simplify code here and there
    - fix memory leaks (unclosed model files)
    - rename stuff in order for it to comply to the Python way,
      e.g. isUrl -> is_url

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,59 +1,74 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import markovify
 import os
 import requests
-from bs4 import BeautifulSoup
-from markov import *
 import urllib.parse as urlparse
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-if(os.getenv('DB_PATH')):
-    print("[*] Building models...")
-    models = make_models_from_path(os.getenv('DB_PATH'), 'messages', 1, True)
-    print(f"[*] Built models {models.keys()}")
+import markov
 
-#help command
+
+_MODELS = None
+
+
 def help(update: Update, context: CallbackContext):
+    """ Help command """
     update.message.reply_text(
-    """
+        """
     Usage:
     /gen [[model].json | url] (number of max chars)
     Example: /gen gnu.org
-    """)
+    """
+    )
+
 
 def gen(update: Update, context: CallbackContext):
     """generate chain based on url"""
-    if(context.args):
+    if context.args:
         content = context.args[0]
         length = 240
-    if(len(context.args) == 2):
+    if len(context.args) == 2:
         length = int(context.args[1])
 
-    print(f"[{update.message.from_user.username}] content: {content}, length: {int(length)}")
+    print(
+        f"[{update.message.from_user.username}] content: {content}, length: {int(length)}"
+    )
 
-    if(urlparse.urlparse(content)):
+    if urlparse.urlparse(content):
         try:
-            output = random_sentence(make_model_from_url(content, 1, False), length)
+            output = markov.BeautifulSouprandom_sentence(
+                markov.BeautifulSoupmake_model_from_url(content, 1, False), length
+            )
         except requests.exceptions.MissingSchema:
-            output = random_sentence(make_model_from_url("http://" + content, 1, False), length)
+            output = markov.random_sentence(
+                markov.make_model_from_url("http://" + content, 1, False), length
+            )
         except IndexError:
             output = "Invalid arguments"
         except requests.exceptions.SSLError:
             output = "Bad handshake"
-            
+
     else:
         try:
-            output = random_sentence(models[content], length)
+            output = markov.random_sentence(_MODELS[content], length)
         except IndexError:
             output = "Invalid arguments"
-    
+
     update.message.reply_text(output)
 
 
+def main():
+    if db_path := os.getenv("DB_PATH"):
+        print("[*] Building models...")
+        global _MODELS
+        _MODELS = markov.make_models_from_path(db_path, "messages", 1, True)
+        print(f"[*] Built models {_MODELS.keys()}")
 
-updater = Updater(os.getenv('BOT_KEY'))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(CommandHandler('gen', gen))
-#updater.dispatcher.add_handler(CommandHandler('gen_url', gen_url))
-updater.start_polling()
-updater.idle()
+    updater = Updater(os.getenv("BOT_KEY"))
+    updater.dispatcher.add_handler(CommandHandler("help", help))
+    updater.dispatcher.add_handler(CommandHandler("gen", gen))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
--- bot/markov.py
+++ bot/markov.py
@@ -1,33 +1,37 @@
-import markovify
 import json
 import re
-import random
 import os
+
 import requests
+import markovify
 from bs4 import BeautifulSoup
 
 
 def list_to_string(db):
     """convert a list of strings into a single one"""
-    texts = []
-    for message in db:
-        if('text' in message and isinstance(message['text'], str)):
-            texts.append(message['text'])
-    return ' '.join(texts)
+    texts = [
+        message["text"]
+        for message in db
+        if "text" in message and isinstance(message["text"], str)
+    ]
+    return " ".join(texts)
+
 
 def parse_text(text, rules):
     """subtract text based on regex rules"""
     for match in rules:
-        parsed = re.sub(match, '', list_to_string(text))
+        parsed = re.sub(match, "", list_to_string(text))
     return parsed
+
 
 def random_sentence(model, length):
     """generate random sentence given a model and length"""
     output = ""
-    while(not output):
+    while not output:
         output = model.make_short_sentence(int(length), tries=100)
     return output
- 
+
+
 def make_models_from_path(path, key, state_size, well_formed):
     """returns a dictionary of 'command': model pairs given a list of filenames"""
     models = []
@@ -35,35 +39,42 @@ def make_models_from_path(path, key, state_size, well_formed):
     file_names = os.listdir(path)
 
     for file_name in file_names:
-
         try:
-            #open and parse the json file
-            db = json.loads(open(path + '/' + file_name).read())[key]
+            with open(path + "/" + file_name) as model_file:
+                # open and parse the json file
+                content = json.loads(model_file.read())[key]
         except KeyError:
             print("Could not build some files, skipping...")
-        
-        #append new model and command to their respective lists
+
+        # append new model and command to their respective lists
         commands.append(file_name)
-        models.append(markovify.NewlineText(list_to_string(db), state_size = state_size, well_formed = well_formed)) 
+        models.append(
+            markovify.NewlineText(
+                list_to_string(content), state_size=state_size, well_formed=well_formed
+            )
+        )
 
     return dict(zip(commands, models))
+
 
 def make_model_from_url(url, state_size, well_formed):
     """generate model from web page content"""
     content = requests.get(url).content
 
-    if(bool(BeautifulSoup(content, "html.parser").find())):
+    if bool(BeautifulSoup(content, "html.parser").find()):
         text_content = BeautifulSoup(content, features="lxml").text
     else:
         text_content = content
 
-    return markovify.NewlineText(text_content, state_size = state_size, well_formed = well_formed) 
+    return markovify.NewlineText(
+        text_content, state_size=state_size, well_formed=well_formed
+    )
 
-def isUrl(str):
-    if(re.search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str)):
-        return True
-    return False
 
-#regex match
-matches = [r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', r'(\<.*?\>)', r'\>']
-
+def is_url(url):
+    return bool(
+        re.search(
+            r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+            url,
+        )
+    )
